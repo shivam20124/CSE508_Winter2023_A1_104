dfa53d609959edc0f5c7a4cf5a93663c462f81a3
Akshita <akshianugupta@gmail.com>
2023-02-16 18:58:54 +0530

    Unigram and Bigram Inverted Indices

--- Loading_pickle.py
+++ Loading_pickle.py
@@ -1,6 +1,6 @@
 import pickle
 
-positional_index = open('D:/College/SEMESTER 6/IR/CSE508_Winter2023_A1_104/Positional_Index_File', 'rb')     
+positional_index = open('C:\Users\AKSHITA\OneDrive\Desktop\IIITD\Sem6\IR\CSE508_Winter2023_A1_104\Positional_Index_File', 'rb')     
 word_dict = pickle.load(positional_index)
 # for words in word_dict:
     # print(words, '=>', word_dict[words])
